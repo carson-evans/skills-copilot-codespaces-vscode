36e64d69e7f6836c20c5db3c47b76454e8341cf2
Carson Evans <carsonevans@outlook.com>
2023-11-28 18:16:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberInfo = document.getElementById("memberInfo");
+    var skillsInfo = document.getElementById("skillsInfo");
+    memberInfo.style.display = "block";
+    skillsInfo.style.display = "none";
+    member.style.backgroundColor = "#2b2a2a";
+    skills.style.backgroundColor = "#1e1e1e";
+}
